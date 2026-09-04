22753e8a042c6791a289cbef2ba6607b8c07a9ce
sugasri036 <sugasrimuraleedharan@gmail.com>
2026-09-04 10:56:36 +0530

    Fix fund search for PostgreSQL

--- src/main/java/com/internship/fundservice/repository/FundRepository.java
+++ src/main/java/com/internship/fundservice/repository/FundRepository.java
@@ -1,7 +1,6 @@
 package com.internship.fundservice.repository;
 
 import com.internship.fundservice.entity.Fund;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
